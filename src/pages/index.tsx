e6a1ea043f1cc6cecc23689c7cb9e36d8a9a8b5b
unknown <sithunaing8209@gmail.com>
2025-03-03 01:01:44 +0630

    fixed error again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import { useRouter } from "next/router";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
 
