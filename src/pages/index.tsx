bf812783bec45ebdfe3e4773576a64c41ef42614
unknown <sithunaing8209@gmail.com>
2025-03-03 00:58:45 +0630

    error fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
 
 const geistSans = Geist({
